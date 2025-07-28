a6a735de4ab8726a8e034a2a8c2a686680c55408
Nutthapon Sangtai <thebankhero@gmail.com>
2025-07-28 20:31:55 +0700

    Setting Up TailwindCSS, Shadcn-Vue, and Automatic Component Imports

--- README.md
+++ README.md
@@ -11,8 +11,40 @@ npm init adonisjs@latest building-with-adonisjs-and-inertia
 - Vue 3
 - SSR true
 
+### TailwindCSS
+npm i -D tailwindcss@3 autoprefixer
+
 ### Shadcn-Vue
 - https://shadcn-vue.com/docs/installation.html
 
-## TailwindCSS
+```code
+npx shadcn-vue@radix init
+```
+- TypeScript > yes
+- framework > Vite
+- style > Default
+- base color > Slate
+- tsconfig.json > ./inertia/tsconfig.json
+- global CSS > ./inertia/css/app.css
+- CSS variables > yes
+- tailwind.config > tailwind.config
+- components > ~/components
+- utils > ~/lib/utils
+- components.json > Y
 
+#### ลบ array ภายใน content ของไฟล์ tailwind.config.js และเพิ่มข้อมูลดังต่อไปนี้
+- './inertia/{pages,components,app,layouts}/**/*.{ts,tsx,vue}',
+- './resources/views/**/*.edge'
+
+#### edit module.exports
+- >>del module.exports = {}
+- >>add export default {}
+
+#### change require to import
+- >>del const animate = require("tailwindcss-animate")
+- >>add import animate from 'tailwindcss-animate'
+
+### Shadcn-Vue Components
+```code
+npx shadcn-vue@radix add button
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,33 @@
-import { defineConfig } from 'vite'
 import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
-import vue from '@vitejs/plugin-vue'
 import adonisjs from '@adonisjs/vite/client'
+import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }), vue(), adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: true, entrypoint: 'inertia/app/ssr.ts' } }),
+    vue(),
+    adonisjs({ entrypoints: ['inertia/app/app.ts'], reload: ['resources/views/**/*.edge'] }),
+    Components({
+      dirs: ['inertia/components'],
+      dts: true,
+    }),
+  ],
+
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
 
   /**
    * Define aliases for importing modules from
    * your frontend code
+   * import Header from '~/components/Header.vue
    */
   resolve: {
     alias: {
