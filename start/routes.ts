27748778e6d67bcb0df2ebe9f819e2abd74e4e82
Nutthapon Sangtai <thebankhero@gmail.com>
2025-07-29 21:02:12 +0700

    Partial and Lazy Data Loading and Evaluation

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,11 @@
 
 import router from '@adonisjs/core/services/router'
 
+let renderCount = 0
+let refreshCount1 = 0
+let refreshCount2 = 0
+let lazyCount = 0
+
 router.on('/').renderInertia('home')
 
 router.get('/register', async (ctx) => {
@@ -19,7 +24,12 @@ router.get('/register', async (ctx) => {
 })
 
 router.get('/login', async (ctx) => {
-  return ctx.inertia.render('auth/login')
+  return ctx.inertia.render('auth/login', {
+    renderCount: ++renderCount,
+    refreshCount1: () => ++refreshCount1,
+    refreshCount2: () => ++refreshCount2,
+    lazyCount: ctx.inertia.lazy(() => ++lazyCount),
+  })
 })
 
 router.post('/register', async (ctx) => {
